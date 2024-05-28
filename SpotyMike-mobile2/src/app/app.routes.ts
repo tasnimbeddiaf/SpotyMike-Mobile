0f1f5329ca878c3ae3974236ce9d3a6e2cfc3c48
Namory <nnamorykanate5@gmail.com>
2024-05-28 22:03:04 +0200

    add layout for login

--- SpotyMike-mobile2/src/app/app.routes.ts
+++ SpotyMike-mobile2/src/app/app.routes.ts
@@ -2,7 +2,32 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    path: 'auth',
+    loadComponent: () => import('./layout/auth/auth.page').then( m => m.AuthPage),
+    children:[
+      {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+      },
+    ]
   },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full'  // Add pathMatch property
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./shared/tabs/tabs.page').then(m => m.TabsPage),
+  },
+
+
+
+
+
 ];
---
+++ SpotyMike-mobile2/src/app/layout/auth/auth.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.page.html',
+  styleUrls: ['./auth.page.scss'],
+  standalone: true,
+  imports: [IonCol, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class AuthPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
