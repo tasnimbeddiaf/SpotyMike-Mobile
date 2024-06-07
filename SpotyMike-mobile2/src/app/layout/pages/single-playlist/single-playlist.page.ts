68a96424a93695941753347fea403454a8eb2ec3
Tasnim <tasnimbeddiaf@gmail.com>
2024-06-07 11:04:13 +0200

    Added song name component

--- SpotyMike-mobile2/src/app/layout/pages/single-playlist/single-playlist.page.ts
+++ SpotyMike-mobile2/src/app/layout/pages/single-playlist/single-playlist.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { atOutline, heartOutline, home, homeOutline, musicalNote, musicalNoteOutline, pause, person, personOutline, play, shareSocialOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-single-playlist',
   templateUrl: './single-playlist.page.html',
   styleUrls: ['./single-playlist.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class SinglePlaylistPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({ heartOutline, play, pause, personOutline, homeOutline, musicalNoteOutline, shareSocialOutline})
+  }
 
   ngOnInit() {
   }
